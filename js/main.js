dd466b406c5be1ab3212bf7900c3753fc7be0b20
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-03-21 11:00:36 +0200

    add styles to the tours section

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ const swiper = new Swiper('.swiper', {
   },
 
 });
+
+const items = document.querySelectorAll('.tours__item');
+
+items.forEach(element => {
+
+    element.addEventListener('click', () => {
+
+        items.forEach(el => el.classList.remove('tours__item--active'));
+
+        element.classList.add('tours__item--active')
+    });
+
+});
