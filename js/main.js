829652b05b746bd9ef1b984b76d677f313e237b2
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-04-02 22:07:58 +0300

    adapt the slider section

--- js/main.js
+++ js/main.js
@@ -10,6 +10,11 @@ const swiper = new Swiper('.swiper', {
   loop: true,
   effect: 'fade',
 
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
