a750055eb87bb0bc29d5125208f64f53a5463db5
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-04-02 12:30:47 +0300

    add burger menu

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
-const swiper = new Swiper('.swiper', {
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu');
 
-    loop: true,
-    effect: "fade",
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu--active');
+});
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  effect: 'fade',
 
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
 });
 
 const items = document.querySelectorAll('.tours__item');
 
 items.forEach(element => {
+  element.addEventListener('click', () => {
+    items.forEach(el => el.classList.remove('tours__item--active'));
 
-    element.addEventListener('click', () => {
-
-        items.forEach(el => el.classList.remove('tours__item--active'));
-
-        element.classList.add('tours__item--active')
-    });
-
+    element.classList.add('tours__item--active');
+  });
 });
